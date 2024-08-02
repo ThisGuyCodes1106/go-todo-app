aef8a9cc484467b91b3a25a8a05435a26d4f68bf
Stefan Madzarac <stefan.madzarac@bjss.com>
2024-08-02 23:03:59 +0100

    TODO 1.13: minor change

--- main.go
+++ main.go
@@ -9,27 +9,6 @@ import (
 var console = fmt.Println
 
 func main() {
-	
-	todo1 := core.ToDoListItem{
-		Title: 		 "Learn Go",
-		Description: "Read Go documentation and practice examples",
-		Status:      "In Progress",
-	}
-	todo2 := core.ToDoListItem{
-		Title:       "Build a TODO App",
-		Description: "Create a TODO app using Go",
-		Status:      "In Progress",
-	}
-	todo3 := core.ToDoListItem{
-		Title:       "Get on BET365 Project",
-		Description: "Join BET365 as a consultant and join their 5-a-side group",
-		Status:      "Not Started",
-	}
-
-	todoList := core.ToDoList{
-		ToDoList: []core.ToDoListItem{todo1, todo2, todo3},
-	}
-	
-	todoList.CreateAndPopulateJSONFile()
-	console(core.ReadJSONFile())
+	todoList, _ := core.ReadJSONFile()
+	console(todoList)
 }
